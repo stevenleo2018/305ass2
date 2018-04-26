b410bf4ad6f7cbb793510038bc5854155fd49f66
stevenleo2018 <chengzudong@gmai.com>
2018-04-26 17:33:38 +1000

    data.txt
    # Conflicts:
    #	README.md
    #	app/src/main/res/layout/activity_appointment.xml

--- app/src/main/java/com/example/chengzu/sit305assignment2/RevengeActivity.java
+++ app/src/main/java/com/example/chengzu/sit305assignment2/RevengeActivity.java
@@ -8,15 +8,41 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RevengeActivity extends AppCompatActivity {
     String description = " ";
     EditText editText;
     Button btn;
+    Button b_read;
+    TextView tv_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_revenge);
+
+        b_read = (Button) findViewById(R.id.b_read);
+        tv_text = (TextView) findViewById(R.id.tv_text);
+        b_read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = "";
+                try {
+                    InputStream is = getAssets().open("data8.txt");
+                    int size = is.available();
+                    byte[] buffer = new byte[size];
+                    is.read(buffer);
+                    is.close();
+                    text= new String(buffer);
+                } catch (IOException ex){
+                    ex.printStackTrace();
+                }
+                tv_text.setText(text);
+            }
+        });
 
         editText = (EditText) findViewById(R.id.editText);
         btn = (Button) findViewById(R.id.button);
--- app/src/main/java/com/example/chengzu/sit305assignment2/eastActivity.java
+++ app/src/main/java/com/example/chengzu/sit305assignment2/eastActivity.java
@@ -6,13 +6,39 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class eastActivity extends AppCompatActivity {
 
+    Button b_read;
+    TextView tv_text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_east);
+        b_read = (Button) findViewById(R.id.b_read);
+        tv_text = (TextView) findViewById(R.id.tv_text);
+        b_read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = "";
+                try {
+                    InputStream is = getAssets().open("data5.txt");
+                    int size = is.available();
+                    byte[] buffer = new byte[size];
+                    is.read(buffer);
+                    is.close();
+                    text= new String(buffer);
+                } catch (IOException ex){
+                    ex.printStackTrace();
+                }
+                tv_text.setText(text);
+            }
+        });
     }
 
     public void gosurvive(View view) {
